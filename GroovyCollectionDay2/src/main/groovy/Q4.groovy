2296b711df879a125500d2b9101d3d2e470870bc
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-13 00:06:24 +0530

    Q4 First Commit

---
+++ GroovyCollectionDay2/src/main/groovy/Q4.groovy
@@ -0,0 +1,14 @@
+//Q4. Find whether two lists have a common element or not
+List list1=[11, 12, 13, 14]
+
+List list2=[13, 14, 15]
+
+println list1.contains(list2)
+
+
+
+List list11=[11, 12, 13, 14]
+
+List list22=[ 13, 14, 11, 12]
+
+println list11.containsAll(list22)
