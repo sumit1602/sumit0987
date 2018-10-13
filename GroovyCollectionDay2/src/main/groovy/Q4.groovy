603341c83193979ee894d4fc709b57509ab8f1fa
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-14 00:31:14 +0530

    Q4 First Commit

--- GroovyCollectionDay2/src/main/groovy/Q4.groovy
+++ GroovyCollectionDay2/src/main/groovy/Q4.groovy
@@ -1,10 +1,19 @@
 //Q4. Find whether two lists have a common element or not
-List list1=[11, 12, 13, 14]
+List list1=[11, 12, 13, 14, 15]
 
-List list2=[13, 14, 15]
+List list2=[13, 14, 15,22,11,111]
 
-println list1.contains(list2)
+ if(list1.intersect(list2))
+     println "Yes there are common elements present in both list and they are: ${list1.intersect(list2)}"
 
+
+
+
+
+
+
+
+println list1.containsAll(list2)
 
 
 List list11=[11, 12, 13, 14]
@@ -12,3 +21,4 @@ List list11=[11, 12, 13, 14]
 List list22=[ 13, 14, 11, 12]
 
 println list11.containsAll(list22)
+
