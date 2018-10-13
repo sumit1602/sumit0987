568e86b3df41eef5c58ae0bac49a868b5c8b1ebf
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-13 16:20:51 +0530

    Q20 First Commit

---
+++ GroovyCollectionDay2/src/main/groovy/Q20.groovy
@@ -0,0 +1,14 @@
+//Q20. Find if a map contains a particular key.
+
+Map map =[1:"sumit", 2:"amit", 3:"Rohit"]
+
+
+println map.containsKey(2)
+println map.containsKey(8)
+println map.containsKey(00)
+println map.containsKey(-1)
+println map.containsKey(-2)
+println map.containsKey(+8)
+println map.containsValue(+8)
+println map.containsValue("sumit")
+println map.containsValue('Rohit')
