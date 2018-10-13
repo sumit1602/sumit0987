c2e9d70eb126663632532eae758330a924e163cd
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-13 10:11:17 +0530

    Q7 First Commit

---
+++ GroovyCollectionDay2/src/main/groovy/Q7.groovy
@@ -0,0 +1,6 @@
+// Q7. Sort the given list in descending order having distinct elements:
+// [14,12, 11,10, 16, 15,12, 10, 99, 90, 14, 16, 35]
+
+List list = [14,12, 11,10, 16, 15,12, 10, 99, 90, 14, 16, 35]
+
+println list.sort().reverse()
