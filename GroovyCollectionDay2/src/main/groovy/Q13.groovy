0b4638a58bcdec22ab7ea3e3e026437f87d115eb
Sumit1602 <sumitchaudhary39@gmail.com>
2018-10-14 21:13:18 +0530

    Q13 2nd Commit

---
+++ GroovyCollectionDay2/src/main/groovy/Q13.groovy
@@ -0,0 +1,13 @@
+//Q13. Find the number of occurences of a character in a string
+
+//not done
+
+String string="this is a string"
+
+
+ListString=string.toList()
+
+
+println ListString.groupBy({it})
+
+
